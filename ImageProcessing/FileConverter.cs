c712004cf055d30130801de3775938b9d217b9aa
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2022-12-05 22:31:40 +0100

    feat: class library ImageProcessing; refactor: moved to base64 conversion logic to class library; chore: configured dependencies for both the web app and the library

---
+++ ImageProcessing/FileConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Text;
+
+namespace ImageProcessing
+{
+    public static class FileConverter
+    {
+        public static async Task<string> BrowserFileToBase64Async(IBrowserFile file)
+        {
+            var resizedFile = await file.RequestImageFileAsync(
+                file.ContentType,
+                640,
+                500);
+            byte[] buffer = new byte[resizedFile.Size];
+            using (var stream = resizedFile.OpenReadStream())
+            {
+                await stream.ReadAsync(buffer);
+            }
+            var base64BufferString = Convert.ToBase64String(buffer);
+            var base64Builder = new StringBuilder("data:image/png;base64,")
+                .Append(base64BufferString);
+
+            return base64Builder.ToString();
+        }
+    }
+}
