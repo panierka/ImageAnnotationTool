29702880902c3370ea0c4fceafb5eb48794adbd4
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2022-12-06 04:18:21 +0100

    refactor: extraction of byte array creation to dedicated function

--- ImageProcessing/FileConverter.cs
+++ ImageProcessing/FileConverter.cs
@@ -9,16 +9,7 @@ namespace ImageProcessing
     {
         public static async Task<string> BrowserFileToBase64Async(IBrowserFile file)
         {
-            byte[] buffer = new byte[file.Size];
-
-            var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-            await using var fs = new FileStream(path, FileMode.Create);
-            await file.OpenReadStream(file.Size).CopyToAsync(fs);
-
-            fs.Position = 0;
-            await fs.ReadAsync(buffer);
-            fs.Close();
-            File.Delete(path);
+            var buffer = await BrowserFileToByteArray(file);
 
             var base64BufferString = Convert.ToBase64String(buffer);
             var format = file.ContentType;
@@ -26,6 +17,25 @@ namespace ImageProcessing
                 .Append(base64BufferString);
 
             return base64Builder.ToString();
+        }
+
+        public static async Task<byte[]> BrowserFileToByteArray(IBrowserFile file)
+        {
+            byte[] buffer = new byte[file.Size];
+
+            var guid = Guid.NewGuid().ToString();
+            var path = Path.Combine(Path.GetTempPath(), guid);
+
+            await using var fs = new FileStream(path, FileMode.Create);
+            await file.OpenReadStream(file.Size).CopyToAsync(fs);
+
+            fs.Position = 0;
+            await fs.ReadAsync(buffer);
+
+            fs.Close();
+            File.Delete(path);
+
+            return buffer;
         }
     }
 }
