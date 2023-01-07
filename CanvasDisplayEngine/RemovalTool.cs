11d8223d3ceb4b3f7f23ea22603699ed4331fc99
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-01-07 02:04:28 +0100

    feat: removing points from shape

---
+++ CanvasDisplayEngine/RemovalTool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasDisplayEngine
+{
+    public class RemovalTool : IShapeEditingTool
+    {
+        public void PressOnCoordinate(Shape shape, InputEventData inputEvent)
+        {
+            var x = inputEvent.MouseX;
+            var y = inputEvent.MouseY;
+
+            var point = ShapeInterpreter.GetPointWithCoordinates(shape, x, y);
+            
+            if (point is { })
+            {
+                shape.RemovePoint(point);
+            }
+        }
+
+        public void MoveOnCoordinate(Shape shape, InputEventData inputEvent) { }
+
+        public void ReleaseOnCoordinate(Shape shape, InputEventData inputEvent) { }
+    }
+}
