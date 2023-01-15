e8936238587f821d48eb5dcbaf50f4f816bf18cb
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-01-15 23:54:59 +0100

    feat: toolset handler; refactor: centralized placeholders

---
+++ ShapeEditor/ShapeToolset.cs
@@ -0,0 +1,24 @@
+﻿using ShapeEditor.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeEditor
+{
+    public class ShapeToolset
+    {
+        private readonly Dictionary<string, IShapeEditingTool> tools = new();
+
+        public void AddTool(string key, IShapeEditingTool tool)
+        {
+            tools.Add(key, tool);
+        }
+        
+        public IShapeEditingTool? GetTool(string key)
+        {
+            return tools.GetValueOrDefault(key);
+        }
+    }
+}
---
+++ ShapeEditor/ShapeToolsetHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeEditor
+{
+    public class ShapeToolsetHandler
+    {
+        private readonly Dictionary<ShapeEditionType, ShapeToolset> toolsets = new();
+        private ShapeToolset currentToolset = null!;
+
+        private readonly ShapeEditor shapeEditor;
+
+        public ShapeToolsetHandler(ShapeEditor shapeEditor)
+        {
+            this.shapeEditor = shapeEditor;
+        }
+
+        public void AddToolset(ShapeEditionType type, ShapeToolset toolset)
+        {
+            toolsets.Add(type, toolset);
+            currentToolset ??= toolset;
+        }
+
+        public void EquipToolset(ShapeEditionType type)
+        {
+            if (toolsets.TryGetValue(type, out var toolset))
+            {
+                currentToolset = toolset;
+            }
+        }
+
+        public void EquipToolFromCurrentToolset(string key)
+        {
+            var tool = currentToolset.GetTool(key);
+
+            if (tool is { })
+            {
+                shapeEditor.EquipEditingTool(tool);
+            }
+        }
+    }
+}
