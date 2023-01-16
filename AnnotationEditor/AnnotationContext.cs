e05e8c2ef39731e8d69e5fbae5893d1bcd8c9673
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-01-17 00:51:53 +0100

    feat: annotation context wrapper; color provider

--- AnnotationEditor/AnnotationContext.cs
+++ AnnotationEditor/AnnotationContext.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ShapeEditing;
+using ImageAnnotationToolDataAccessLibrary.Models.ImageAnnotation;
+using CanvasDisplayEngine;
 
 namespace AnnotationEditor
 {
     public class AnnotationContext
     {
         private readonly ShapeEditor editor;
+        private readonly Annotation annotation;
+        private readonly IColorProvider colorProvider;
 
+        public Shape Shape { get; }
+
+        private ColorRGB color = null!;
+
+        public AnnotationContext(ShapeEditor editor, Annotation annotation, IColorProvider colorProvider)
+        {
+            this.editor = editor;
+            this.annotation = annotation;
+
+            color = colorProvider.GetNextColor();
+            Shape = new(color);
+            this.colorProvider = colorProvider;
+        }
     }
 }
---
+++ AnnotationEditor/IColorProvider.cs
@@ -0,0 +1,14 @@
+﻿using CanvasDisplayEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationEditor
+{
+    public interface IColorProvider
+    {
+        public ColorRGB GetNextColor();
+    }
+}
