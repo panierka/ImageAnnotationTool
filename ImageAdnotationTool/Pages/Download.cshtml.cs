bc5b818ee54610183e7a7646191d9a1e50aa8aa1
TR1PL3D0T <hubikwiatkowski@gmail.com>
2023-01-24 12:11:49 +0100

    feat: downloading json file

---
+++ ImageAdnotationTool/Pages/Download.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using ImageAnnotationToolDataAccessLibrary.Models.ImageAnnotation.Coco;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using ImageAnnotationToolDataAccessLibrary.JsonFiles;
+
+namespace ImageAnnotationTool.Pages
+{
+	public class DownloadModel : PageModel
+	{
+		private readonly IWebHostEnvironment _env;
+		private readonly Coco _cocoService;
+		public DownloadModel(IWebHostEnvironment env, Coco coco)
+		{
+			_env = env;
+			_cocoService = coco;
+		}
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var serialization = new JsonSerialization<Coco>();
+			var jsonstr = serialization.Serialize(_cocoService);
+			byte[] byteArray = System.Text.ASCIIEncoding.ASCII.GetBytes(jsonstr);
+
+			return File(byteArray, "application/force-download", "coco.json");
+		}
+	}
+}
