9061fbf172a437b25601c8b9158c87c2fc5cda80
TR1PL3D0T <hubikwiatkowski@gmail.com>
2023-01-24 13:41:38 +0100

    update: download json file

--- ImageAdnotationTool/Pages/Download.cshtml.cs
+++ ImageAdnotationTool/Pages/Download.cshtml.cs
@@ -4,22 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 using ImageAnnotationToolDataAccessLibrary.JsonFiles;
+using ImageAnnotationToolDataAccessLibrary.Services;
 
 namespace ImageAnnotationTool.Pages
 {
 	public class DownloadModel : PageModel
 	{
 		private readonly IWebHostEnvironment _env;
-		private readonly Coco _cocoService;
-		public DownloadModel(IWebHostEnvironment env, Coco coco)
+		//private readonly Coco _cocoService;
+		private readonly int _projectId;
+
+		public DownloadModel(IWebHostEnvironment env, int projectId)
 		{
 			_env = env;
-			_cocoService = coco;
+			//_cocoService = coco;
+			_projectId = projectId;
 		}
 		public async Task<IActionResult> OnGetAsync()
 		{
+			//var projectServiceProvider = new ProjectServiceProvider();
+			var coco = new Coco();
 			var serialization = new JsonSerialization<Coco>();
-			var jsonstr = serialization.Serialize(_cocoService);
+			var jsonstr = serialization.Serialize(coco);
 			byte[] byteArray = System.Text.ASCIIEncoding.ASCII.GetBytes(jsonstr);
 
 			return File(byteArray, "application/force-download", "coco.json");
